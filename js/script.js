1e2d989731ddbf7dfc5132255be5d015d8369f02
Jonathan Chang <j.a.chang820@gmail.com>
2018-02-14 04:55:41 -0800

    Changed all functions to old JS style for consistency and to make it less confusing. Restructured JS and added comments to make it more readable. Deleted all unused elements and changed video to audio. Removed setInterval() and setTimeout() with getUserMedia() to prevent continuous user prompts, and since it doesn't work too well at the moment. Updated HTML/CSS to make it more responsive. It should work now for recording 5s mp3 clips without a server.

--- js/script.js
+++ js/script.js
@@ -1,92 +1,124 @@
 const recordingTime = 5000;
-
-var count = 0;
-var stopNow = true;
+var isRecording = false;
 
 function wait() {
   return new Promise(resolve => setTimeout(resolve, recordingTime));
 }
 
-function startRecording(stream) {
-  let recorder = new MediaRecorder(stream);
-  let data = [];
 
-  recorder.ondataavailable = event => data.push(event.data);
-  recorder.start();
-  console.log(recorder.state + " for " + (recordingTime) + " ms...");
-
-  let stopped = new Promise((resolve, reject) => {
-    recorder.onstop = resolve;
-    recorder.onerror = event => reject(event.name);
-  });
-
-  let recorded = wait().then(
-    () => recorder.state == "recording" && recorder.stop()
-  );
-
-  return Promise.all([
-    stopped,
-    recorded
-  ])
-  .then(() => data);
+function showStartButton() {
+  $("#recordButton").addClass("fa-start-circle-o");
+  $("#recordButton").removeClass("fa-stop-circle-o");
 }
 
-function stop(stream) {
-  stream.getTracks().forEach(track => track.stop());
+
+function showStopButton() {
+  $("#recordButton").addClass("fa-stop-circle-o");
+  $("#recordButton").removeClass("fa-start-circle-o");
 }
 
-$("#startButton").addEventListener("click", function() {
-  console.log("button was pressed");
-  stopNow = false;
-  beginRecording();
-  $("#startButton").style.display = "none";
-  $("#stopButton").style.display = "inline";
-  $("#preview").style.visibility = "visible";
-  setTimeout(updateScore, 6000);
-}, false);
 
-$("#stopButton").addEventListener("click", function() {
-  stopNow = true;
-  stop($("#preview").srcObject);
-  $("#startButton").style.display = "inline";
-  $("#stopButton").style.display = "none";
-  $("#preview").style.visibility = "hidden";
+$("#recordButton").on("click", function() {
+  console.log(isRecording);
+  
+  if (isRecording == false) {
 
-}, false);
+    isRecording = true;
 
-function beginRecording() {
-
-  if(!stopNow)
-  {
-    setTimeout(beginRecording, recordingTime);
-
+    // returns Promise that resolves to a MediaStream object
     navigator.mediaDevices.getUserMedia({
+
       audio: true
-    }).then(stream => {
-      $("#preview").srcObject = stream;
-      $("#downloadButton").href = stream;
-      $("#preview").captureStream = $("#preview").captureStream || $("#preview").mozCaptureStream;
-      return new Promise(resolve => $("#preview").onplaying = resolve);
-    }).then(() => startRecording($("#preview").captureStream()))
-    .then (recordedChunks => {
-      let recordedBlob = new Blob(recordedChunks, { type: "audio/webm" });
 
-      //download the recording as a webm
-      $("#recording").src = URL.createObjectURL(recordedBlob);
-      $("#downloadButton").href = $("recording").src;
-      $("#downloadButton").download = "audio_" + count + ".webm";
-      $("#downloadButton").click();
-      count += 1;
+    // to be run after getUserMedia returns
+    // stream: MediaStream object
+    }).then(function(stream) {
 
-      console.log("Successfully recorded " + recordedBlob.size + " bytes of " + recordedBlob.type + " media.");
-    }).catch(console.log("error"));
+      showStopButton();
+
+      // starts recording a new stream
+      let recorder = new MediaRecorder(stream);
+      let data = [];
+
+      /* NOTE:
+         Use a setInterval() with the MediaRecorder.requestData() function,
+         instead of mediaDevices.getUserMedia(). getUserMedia() function
+         will prompt user every time unless the user clicks "Remember this
+         decision". requestData() saves the Blob and continues recording.
+         
+         We can use requestData() to store a Blob with URL.createObjectURL(),
+         then send this temporary file to Google Cloud API. This doesn't save
+         a file on the FS. The file exists only as long as the window is open.
+         We can release the temporary files by URL.revokeObjectURL().
+      */
+
+      // if a stream is available, add the Blob to 'data' and record
+      recorder.ondataavailable = function(event) { data.push(event.data); }
+      recorder.start();
+      console.log(recorder.state + " for " + (recordingTime) + " ms...");
+
+      // Promise resolves when stream is stopped
+      let stopped = new Promise(function(resolve, reject) {
+        recorder.onstop = resolve;
+        recorder.onerror = function(event) { reject(event.name); }
+      });
+
+      // Promise resolves when recording time is up
+      let recorded = wait().then(
+        function() { recorder.state == "recording" && recorder.stop(); }
+      );
+      
+      // Promise resolves when the stream is both stopped and time is up
+      // returns the array of Blobs
+      return Promise.all([
+        stopped,
+        recorded
+      ])
+      .then(function() { 
+          return data; 
+      });
+
+    // record media to file
+    // recordedChunks: array of Blob objects
+    }).then(function(recordedChunks) {
+
+      // combine recorded chunks into one blob
+      let recordedBlob = new Blob(recordedChunks, { type: "audio/mp3" });
+      console.log(recordedBlob);
+
+      // download the recording as a webm
+      $("#recording").attr("src", URL.createObjectURL(recordedBlob));
+
+      // show download button
+      $("#downloadButton").show();
+      $("#downloadButton").attr("href", $("#recording").attr("src"));
+      $("#downloadButton").attr("download", "audio.mp3");
+
+      console.log("Successfully recorded " + recordedBlob.size + 
+        " bytes of " + recordedBlob.type + " media.");
+
+      isRecording = false;
+      showStartButton();
+
+      setTimeout(updateScore, 5000); // test
+
+    }).catch(function(err) {
+
+      console.log("User cancelled media stream.");
+      isRecording = false;
+      showStartButton();
+
+    });
+
+  } else {
+    isRecording = false;
+    showStartButton();
+
   }
-}
+
+});
 
 
 function updateScore() {
   console.log('updateScore is running');
-  var numRand = Math.floor(Math.random() * 31 + 50);
-  $("#score").innerHTML = "Score: " + numRand;
-  setTimeout(updateScore, 5000);
 }
